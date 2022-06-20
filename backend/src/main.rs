c160e7f91900c9d2a5101dfff996d0929244d6a5
bokutotu <mushin.hudoushin@gmail.com>
2022-06-20 21:59:07 +0900

    say hello world

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, HttpResponse};
+
+// struct HelloJson {
+//     content: String
+// }
+
+async fn send_hello() -> HttpResponse {
+    // let hellow_world = HelloJson {
+    //     content: "Hello World".to_string()
+    // };
+    HttpResponse::Ok().body("hello world")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(send_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
