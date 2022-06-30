96af0e8736c47691ae9e85d787e1a0bc0bba3c2e
bokutotu <mushin.hudoushin@gmail.com>
2022-06-30 22:11:41 +0900

    hello world from Browser

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -2,20 +2,82 @@ module Main exposing (..)
 
 -- import Browser
 import Html exposing (..)
--- import Html.Attributes exposing (style)
--- import Html.Events exposing (..)
--- import Http
--- import Json.Decode exposing (Decoder, map4, field, int, string)
+import Html.Attributes exposing (style)
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (Decoder, map4, field, int, string)
 
 -- -- MAIN
 
--- mian = 
---   text "Hello! World!"
 
--- Show a list of items I need to buy at the grocery store.
+-- main =
+--   Browser.element {
+--     init = init,
+--     view = view,
+--     update = update,
+--     subscription = subscription
+--   }
+
+-- type alias Model = Int
+
+-- init: Model -> (Model, Cmd)
+-- init Model = 
+--   (0, Cmd.none)
+
+-- type alias Msg = String
+
+-- view: Msg -> Html Msg
+
+-- Press buttons to increment and decrement a counter.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/architecture/buttons.html
 --
 
 
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+
+-- MAIN
+
+
 main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model = String
+
+
+init : Model
+init =
+  "Hello World"
+
+
+
+-- UPDATE
+
+
+type alias Msg = String
+
+
+update : Msg -> Model -> Model
+update msg model =
+  "Hello World"
+
+
+   -- VIEW
+
+
+view : Model -> Html Msg
+view model =
   div []
-    [ text "Hello, World" ]
+    [
+      text model 
+    ]
